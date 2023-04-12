32bdb7db71cda1878672bc417b96908802397870
zacharydserafin <zacharydserafin@gmail.com>
2023-04-12 15:50:44 -0500

    Added Delete Route

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.get('/api/notes', (req, res) => {
     fs.readFile('./db/db.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
-            res.status(501).json(err);
+            res.status(500).json(err);
         } else {
             res.status(200).json(data);
             return;
@@ -58,6 +58,27 @@ app.post('/api/notes', (req, res) => {
     } else {
         res.status(500).json('Error in posting note');
     }
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    fs.readFile('./db/db.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            res.status(500).json(err);
+            return;
+        } else {
+            const notes = JSON.parse(data);
+            const newNotes = JSON.stringify(notes.filter(note => note.id !== req.params.id), null, 4);
+            
+            fs.writeFile('./db/db.json', newNotes, 
+                    (writeErr) =>
+                        writeErr
+                            ? console.error(writeErr)
+                            : console.info('Successfully rewrote db.json')
+            );
+            res.status(200).send();
+        }
+    });
 });
 
 app.get('*', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
